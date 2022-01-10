91684a82daa7772d8e8ba83081e422235409014d
Lukas Stauersbøl <lukas.stauersbol@gmail.com>
2022-01-10 08:31:16 +0100

    Re initial
    TypeScript seemed a little confusing

---
+++ travellog-website/components/NavBar.js
@@ -0,0 +1,8 @@
+import Link from 'next/link';
+export default function NavBar() {
+    return (
+        <Link href="/a">
+            <a>TEST</a>
+        </Link>
+    )
+}
--- travellog-website/pages/index.js
+++ travellog-website/pages/index.js
@@ -1,9 +1,9 @@
-import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import NavBar from '../components/NavBar'
 
-const Home: NextPage = () => {
+export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
@@ -13,13 +13,14 @@ const Home: NextPage = () => {
       </Head>
 
       <main className={styles.main}>
+        <NavBar></NavBar>
         <h1 className={styles.title}>
           Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
 
         <p className={styles.description}>
           Get started by editing{' '}
-          <code className={styles.code}>pages/index.tsx</code>
+          <code className={styles.code}>pages/index.js</code>
         </p>
 
         <div className={styles.grid}>
@@ -68,5 +69,3 @@ const Home: NextPage = () => {
     </div>
   )
 }
-
-export default Home
