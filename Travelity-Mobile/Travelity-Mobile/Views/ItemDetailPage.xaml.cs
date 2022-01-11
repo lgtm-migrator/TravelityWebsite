f68fb5273b2dd396dfcc9e5731843a9aaa33b2a0
Manish946 <50454163+Manish946@users.noreply.github.com>
2022-01-11 10:36:46 +0100

    Initial

---
+++ Travelity-Mobile/Travelity-Mobile/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+using Travelity_Mobile.ViewModels;
+using Xamarin.Forms;
+
+namespace Travelity_Mobile.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
