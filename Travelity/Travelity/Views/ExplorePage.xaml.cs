9e2522b4414beacb7dc75ab9ad47e2081dfebe53
Manish946 <50454163+Manish946@users.noreply.github.com>
2022-01-12 13:58:39 +0100

    Navigation of 3 Tab Updated.

---
+++ Travelity/Travelity/Views/ExplorePage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Travelity.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ExplorePage : ContentPage
+    {
+        public ExplorePage()
+        {
+            InitializeComponent();
+            NavigationPage.SetTitleIconImageSource(this, "icon_home.png");
+        }
+    }
+}
