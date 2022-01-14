f365b4de8d96095b7640ef4099d9394b672099e6
Lukas Stauersbøl <lukas.stauersbol@gmail.com>
2022-01-14 09:57:08 +0100

    Added Vue Router

--- src/main.js
+++ src/main.js
@@ -3,10 +3,17 @@ import App from './App.vue'
 import vuetify from './plugins/vuetify'
 import { loadFonts } from './plugins/webfontloader'
 import VueSmoothScroll from 'v-smooth-scroll'
+import {createRouter, createWebHistory} from 'vue-router'
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes: []
+})
 
 loadFonts()
 
 createApp(App)
   .use(vuetify)
   .use(VueSmoothScroll)
+  .use(router)
   .mount('#app')
