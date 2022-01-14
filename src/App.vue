e3edd8adec0a2f6c95ff73cc7e27b5925de90024
Lukas Stauersbøl <lukas.stauersbol@gmail.com>
2022-01-14 12:37:24 +0100

    Preping for NavBar

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,22 @@
 <template>
   <v-app>
     <v-main>
-      <HelloWorld/>
+    <NavBar/>
     </v-main>
   </v-app>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import NavBar from './components/NavBar.vue'
 
 export default {
   name: 'App',
 
   components: {
     HelloWorld,
-  },
+    NavBar
+},
 
   data: () => ({
     //
---
+++ src/components/NavBar.vue
@@ -0,0 +1,21 @@
+<template>
+    <nav>
+        <div id="navbar">
+        <p>Travelity</p>
+        </div>
+        <div id="navbar-right">
+            <a href="#home">Home</a>
+            <a href="#about">About</a>
+            <a href="#whyus">Why Us</a>
+            <a href="#team">The Team</a>
+        </div>
+    </nav>
+</template>
+<script>
+export default {
+
+}
+</script>
+<style lang="scss">
+
+</style>
