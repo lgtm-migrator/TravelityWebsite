8e96b0d2adb9875ec4417c80fd2105d0a2f34a71
Lukas Stauersbøl <lukas.stauersbol@gmail.com>
2022-01-14 08:30:17 +0100

    Added Smooth scroll

--- src/main.js
+++ src/main.js
@@ -3,8 +3,10 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import vuetify from './plugins/vuetify'
+import VueSmoothScroll from 'vue2-smooth-scroll'
 
 Vue.config.productionTip = false
+Vue.use(VueSmoothScroll)
 
 new Vue({
   router,
