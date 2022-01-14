47833c47d97b3fc9f8cddf13484088ed00b25744
Lukas Stauersbøl <lukas.stauersbol@gmail.com>
2022-01-14 09:40:21 +0100

    Readded GSAP Loco and Smooth Scroll
    Should have added what was there before.

--- src/main.js
+++ src/main.js
@@ -2,9 +2,11 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import vuetify from './plugins/vuetify'
 import { loadFonts } from './plugins/webfontloader'
+import VueSmoothScroll from 'v-smooth-scroll'
 
 loadFonts()
 
 createApp(App)
   .use(vuetify)
+  .use(VueSmoothScroll)
   .mount('#app')
