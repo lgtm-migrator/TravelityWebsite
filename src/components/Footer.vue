bfa9dbff71d20244e7bbc7d9ea5f172e542c616a
Lukas Stauersbøl <lukas.stauersbol@gmail.com>
2022-01-18 13:14:37 +0100

    feat: Added Footer
    Added footer component

---
+++ src/components/Footer.vue
@@ -0,0 +1,18 @@
+<template>
+  <span class="footer-text">
+    &copy; 2022 - {{ new Date().getFullYear() }} - Travelity
+  </span>
+</template>
+<script>
+export default {};
+</script>
+<style scoped>
+.footer-text {
+  font-family: "Karla", sans-serif;
+  font-size: 12px;
+  color: black;
+  text-align: center;
+  padding: 5px;
+  letter-spacing: 2px;
+}
+</style>
