1cb19fb218febbfdb01239ca06300a694d14a497
Lukas Stauersbøl <lukas.stauersbol@gmail.com>
2022-02-10 14:20:46 +0100

    chore: Removed unused code

--- src/main.js
+++ src/main.js
@@ -3,13 +3,6 @@ import App from './App.vue'
 import vuetify from './plugins/vuetify'
 import { loadFonts } from './plugins/webfontloader'
 import VueSmoothScroll from 'v-smooth-scroll'
-import { createRouter, createWebHistory } from 'vue-router'
-
-const router = createRouter({
-  history: createWebHistory(),
-  routes: []
-})
-
 
 loadFonts()
 
