d49d45573aceab9355ed52cf6f4f712ea757293b
Lukas Stauersbøl <lukas.stauersbol@gmail.com>
2022-02-22 10:17:11 +0100

    feat?: Finally made carousel work

--- src/components/TeamCarousel.vue
+++ src/components/TeamCarousel.vue
@@ -1,5 +1,22 @@
-<template lang=""><div> </div></template>
+<template>
+  <Splide :options="{ rewind: true, focus: 'center', width: '400px' }">
+    <SplideSlide>
+      <img src="https://via.placeholder.com/400" />
+    </SplideSlide>
+    <SplideSlide>
+      <img src="https://via.placeholder.com/400" />
+    </SplideSlide>
+  </Splide>
+</template>
 <script>
-export default {};
+import { Splide, SplideSlide } from "@splidejs/vue-splide";
+import "@splidejs/splide/dist/css/splide.min.css";
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  components: {
+    Splide,
+    SplideSlide,
+  },
+});
 </script>
-<style lang=""></style>
