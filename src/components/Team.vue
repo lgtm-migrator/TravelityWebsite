77aed1a645c9525defef2492b41beb137fdca73a
Lukas Stauersbøl <lukas.stauersbol@gmail.com>
2022-03-17 10:52:56 +0100

    refactor: redid for responsiveness

--- src/components/Team.vue
+++ src/components/Team.vue
@@ -6,17 +6,23 @@
         <TeamBoxesV2 />
       </v-col>
       <v-col class="height-100" cols="12" sm="8" md="4" lg="5" align="center">
-        <Contact />
+        <div class="d-none d-sm-flex">
+          <Contact />
+        </div>
+        <div class="d-flex d-sm-none justify-center align-center">
+          <MobileContact />
+        </div>
       </v-col>
     </v-row>
   </v-container>
 </template>
 <script>
+import MobileContact from './MobileContact.vue';
 import TeamBoxesV2 from './TeamBoxesV2.vue';
 import TeamBoxes from './TeamBoxes.vue';
 import Contact from './ContactForm.vue';
 export default {
-  components: { Contact, TeamBoxes, TeamBoxesV2 },
+  components: { Contact, TeamBoxes, TeamBoxesV2, MobileContact },
   data() {
     return {
       //
