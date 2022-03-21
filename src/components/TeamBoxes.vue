3993811ab1d8ea9813eab1bf0f78961b0a4a1741
Lukas Stauersbøl <lukas.stauersbol@gmail.com>
2022-03-21 09:01:10 +0100

    refactor: restyling code and renaming components

--- src/components/TeamBoxes.vue
+++ src/components/TeamBoxes.vue
@@ -1,181 +1,190 @@
 <template>
-  <div class="tile-container">
-    <div v-for="(k, item) in persons" :key="k" :class="'tile ' + 'person-' + item">
-      <p>{{ k.name }}</p>
+  <div class="container">
+    <div class="cards card-1">
+      <div class="cards__inner"></div>
+      <div class="card-info">
+        <p>Lukas</p>
+        <a href="https://github.com/Slash1y"><v-icon>mdi-github</v-icon></a>
+      </div>
     </div>
-  </div>
-  <div class="page-container">
-    <div v-for="(i, item) in persons" :key="i" :class="'page ' + 'person-' + item"></div>
+    <div class="cards card-2">
+      <div class="cards__inner"></div>
+      <div class="card-info">
+        <p>Manish</p>
+        <a href="https://github.com/Manish946"><v-icon>mdi-github</v-icon></a>
+      </div>
+    </div>
+    <div class="cards card-3">
+      <div class="cards__inner"></div>
+      <div class="card-info">
+        <p>Nicolaj</p>
+        <a href="https://github.com/Nicolaj-S"><v-icon>mdi-github</v-icon></a>
+      </div>
+    </div>
+    <div class="cards card-4">
+      <div class="cards__inner"></div>
+      <div class="card-info">
+        <p>Nirakash</p>
+        <a href="https://github.com/CyberNira"><v-icon>mdi-github</v-icon></a>
+      </div>
+    </div>
+
+    <div class="cards card-5">
+      <div class="cards__inner"></div>
+      <div class="card-info">
+        <p>Owen</p>
+        <a href="https://github.com/RiceBoyy"><v-icon>mdi-github</v-icon></a>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import gsap from 'gsap';
 export default {
-  created() {},
   data() {
     return {
-      persons: [
-        {
-          name: 'Lukas',
-        },
-        {
-          name: 'Owen',
-        },
-        {
-          name: 'Manish',
-        },
-        {
-          name: 'Nikolaj',
-        },
-      ],
+      //
     };
   },
 
-  mounted() {
-    let pages = document.querySelectorAll('.page');
-    let tiles = document.querySelectorAll('.tile');
-    for (let i = 0; i < tiles.length; i++) {
-      this.addListeners(tiles[i], pages[i]);
-    }
-  },
-  props: {},
   methods: {
-    addListeners(tile, page) {
-      let root = document.documentElement;
-      let body = document.body;
-
-      page.addEventListener('click', function () {
-        animateHero(page, tile);
-      });
-      tile.addEventListener('click', function () {
-        animateHero(tile, page);
-      });
-
-      function calculatePosition(element) {
-        let rect = element.getBoundingClientRect();
-
-        let scrollTop = window.scrollY || root.scrollTop || body.scrollTop || 0;
-        let scrollLeft = window.scrollX || root.scrollLeft || body.scrollLeft || 0;
-
-        let clientTop = root.clientTop || body.clientTop || 0;
-        let clientLeft = root.clientLeft || body.clientLeft || 0;
-
-        return {
-          top: Math.round(rect.top + scrollTop - clientTop),
-          left: Math.round(rect.left + scrollLeft - clientLeft),
-          height: rect.height,
-          width: rect.width,
-        };
-      }
-
-      function animateHero(fromHero, toHero) {
-        let clone = fromHero.cloneNode(true);
-        let from = calculatePosition(fromHero);
-        let to = calculatePosition(toHero);
-
-        gsap.set([fromHero, toHero], { visibility: 'hidden', opacity: 0 });
-        gsap.set(clone, {
-          position: 'absolute',
-          margin: 0,
-        });
-
-        body.appendChild(clone);
-
-        gsap.set(clone, from);
-        gsap.to(clone, {
-          duration: 0.5,
-          x: to.left - from.left,
-          y: to.top - from.top,
-          width: to.width,
-          height: to.height,
-          autoRound: false,
-          ease: 'Power1.easeOut',
-          zIndex: 9999999999999,
-          onComplete: onComplete,
-        });
-
-        function onComplete() {
-          if (toHero.classList.contains('page')) {
-            gsap.set(toHero, {
-              delay: 0.1,
-              duration: 0.5,
-              opacity: 1,
-              ease: 'Power1.easeIn',
-            });
-            gsap.set('.page-container', {
-              duration: 0.5,
-              opacity: 1,
-            });
-            gsap.set(toHero, {
-              visibility: 'visible',
-            });
-          } else if (toHero.classList.contains('tile')) {
-            gsap.set(toHero, {
-              delay: 0.1,
-              duration: 0.5,
-              opacity: 1,
-            });
-            gsap.set('.page-container', {
-              duration: 0.5,
-              opacity: 1,
-            });
-            gsap.set(toHero, {
-              visibility: 'visible',
-            });
-          }
-          setTimeout(() => {
-            console.log('die start');
-            body.removeChild(clone);
-            console.log('die end');
-          }, 250);
-        }
-      }
+    setClasses() {
+      const cards = document.querySelectorAll('.cards');
+      const classes = ['left2', 'left', 'active', 'right2', 'right'];
+      cards.forEach((card, index) => card.classList.add(classes[index]));
     },
+
+    changePosition(e) {
+      const clickedCard = e.currentTarget;
+      const activeCard = document.querySelector('.cards.active');
+      if (clickedCard.classList.contains('active')) return;
+      const classesFrom = e.currentTarget.className;
+      const classesTo = activeCard.className;
+      clickedCard.className = classesTo;
+      activeCard.className = classesFrom;
+    },
+  },
+
+  mounted() {
+    const cards = document.querySelectorAll('.cards');
+    cards.forEach((item) => {
+      item.addEventListener('click', this.changePosition);
+    });
+    this.setClasses();
   },
 };
 </script>
 
 <style lang="scss" scoped>
-.tile-container {
+.container {
+  max-width: 800px;
+  margin: auto;
   display: flex;
   justify-content: center;
-  width: 100%;
-  text-align: center;
-  user-select: none;
+  margin-top: 2em;
+  position: relative;
 }
 
-.tile {
-  width: 400px;
-  height: 400px;
-  max-width: 100%;
-  margin: 4px;
-  cursor: pointer;
-  display: inline-block;
-}
-
-.page-container {
-  visibility: hidden;
-  opacity: 0;
-}
-
-.page {
-  cursor: pointer;
+.cards {
+  border-radius: 10px;
+  box-shadow: 0px 0px 30px rgba(black, 0.2);
   position: absolute;
-  height: 100vh;
-  width: 100vw;
-  top: 0;
-  left: 0;
-  position: fixed;
-  z-index: 10000000000;
+  width: 33.33%;
+  transition: all 400ms ease;
+  background-size: cover;
+  background-position: center;
+
+  &:hover:not(.active) {
+    &.left {
+      z-index: 4;
+      transform: translateX(-50%) rotate(-10deg) scale(0.9);
+    }
+
+    &.right {
+      z-index: 4;
+      transform: translateX(50%) rotate(10deg) scale(0.9);
+    }
+    &.left2 {
+      z-index: 3;
+      transform: translateX(-100%) rotate(-10deg) scale(0.9);
+    }
+
+    &.right2 {
+      z-index: 3;
+      transform: translateX(100%) rotate(10deg) scale(0.9);
+    }
+  }
+
+  &.left {
+    z-index: 4;
+    transform: translateX(-50%) scale(0.9);
+  }
+
+  &.right {
+    z-index: 4;
+    transform: translateX(50%) scale(0.9);
+  }
+  &.left2 {
+    z-index: 3;
+    transform: translateX(-100%) scale(0.8);
+  }
+
+  &.right2 {
+    z-index: 3;
+    transform: translateX(100%) scale(0.8);
+  }
+
+  &.active {
+    z-index: 5;
+    position: relative;
+  }
+
+  &__inner {
+    position: relative;
+    padding-bottom: calc(675 / 425) * 100%;
+  }
 }
 
-@for $i from 0 through 4 {
-  .person-#{$i} {
-    background: center 10% no-repeat white;
-    background-size: auto;
-    opacity: 1;
-    background-image: url('/src/assets/person#{$i}.png');
+.card-1 {
+  background-image: url('https://i.ibb.co/30kV8nx/person0.png');
+  background-color: #fff;
+}
+.card-2 {
+  background-image: url('https://i.ibb.co/30kV8nx/person0.png');
+  background-color: #fff;
+}
+.card-3 {
+  background-image: url('https://i.ibb.co/30kV8nx/person0.png');
+  background-color: #fff;
+}
+.card-4 {
+  background-image: url('https://i.ibb.co/30kV8nx/person0.png');
+  background-color: #fff;
+}
+.card-5 {
+  background-image: url('https://i.ibb.co/30kV8nx/person0.png');
+  background-color: #fff;
+}
+
+.card-info {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  height: 2.5rem;
+  background-color: #fff;
+  border-end-end-radius: 10px;
+  border-end-start-radius: 10px;
+  text-align: left;
+  padding: 0.5em;
+  font-family: 'Karla', sans-serif;
+  a {
+    text-decoration: none;
+    color: black;
+    transition: 0.2s ease;
+  }
+  a:hover {
+    color: rgb(155, 153, 153);
   }
 }
 </style>
