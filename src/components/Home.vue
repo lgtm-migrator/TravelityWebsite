b98d195f5346cb7ae6f93b5eeaceae20cb8d3c86
Lukas Stauersbøl <lukas.stauersbol@gmail.com>
2022-04-07 08:04:51 +0200

    fix: fix unused variable

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -25,8 +25,9 @@ export default {
 
   methods: {
     HomePara: function () {
+      let pInstance;
       let scene = document.getElementById('scene');
-      let pInstance = new Parallax(scene, {
+      pInstance = new Parallax(scene, {
         relativeInput: true,
       });
     },
