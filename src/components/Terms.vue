035ba61d908e45434260848380094376a900166a
Lukas Stauersbøl <lukas.stauersbol@gmail.com>
2022-05-03 08:56:12 +0200

    feat: added tons of text for terms

--- src/components/Terms.vue
+++ src/components/Terms.vue
@@ -1,5 +1,211 @@
 <template>
-  <div>HEJ</div>
+  <div class="terms">
+    <p class="header">Terms and Conditions</p>
+    <p>
+      Note: that the Terms and Conditions may be updated in the future to help our users
+      to understand what they may and may not do for their own safety and experiences on
+      our services.
+    </p>
+    <p class="underline">
+      This section describes our relationship with you. It contains a description of
+      Travelity, defines our Agreement, and indicates the name of your service provider.
+    </p>
+    <p>
+      By using this app
+      <span class="bold">we assume you accept these terms and conditions.</span> Do not
+      continue to use the Service (“Travelity”) if you do not agree to take all of the
+      terms and conditions stated on this page.
+    </p>
+    <br />
+    <p>
+      The following terminology applies to these Terms and Conditions, Privacy Statement
+      and Disclaimer Notice and all Agreets: “Client”, “You”, and “Your” refers to you,
+      the person log on this website and compliant to the Service’s terms and conditions.
+      “The Service”, “Ourselves”, “We”, “Our”, and “Us”, refers to our Service. “Party”,
+      “Parties”, or “Us”, refers to both the Client and ourselves. All terms refer to the
+      offer, acceptance and consideration of payment necessary to undertake the process of
+      our assistance to the Client in the most appropriate manner for the express purpose
+      of meeting the Client’s needs in respect of provision of the Service’s stated
+      service, in accordance with and subject to, prevailing law of Netherlands. Any use
+      of the terminology or other words in the singular, plural, capitalization and/or
+      he/she or they, are taken as interchangeable and therefore as referring to the same.
+    </p>
+    <br />
+    <p class="header">Age Requirement</p>
+    <p>
+      If you’re under the age of 18, you must have your legal guardian’s permission to use
+      the Service. Please have your legal guardian read these terms with you. If you’re a
+      legal guardian, and you allow your child to use the services, then these Agreements
+      apply to you and you’re responsible for your child’s activity on the Service.
+    </p>
+    <br />
+    <p class="header">Content of the Service</p>
+    <p>
+      The content of our services is provided by the users of Travelity, our services can
+      contain images, videos, comments and hyperlinks. You stand reliable for how you
+      interact with any of these, we do not stand reliable for how the user interacts with
+      any of these. Travelity may also advertise other companies to keep up our services,
+      and also to improve user experience.
+    </p>
+    <br />
+    <p class="header">Content on the Service</p>
+    <p>
+      You stand responsible for your use of our services and for the content that you
+      provide, including compliance with applicable laws, rules and regulations. You
+      should only provide content that you are comfortable sharing with other users. You
+      are solely responsible for any use or reliance on any content or materials posted
+      via our services or accessed through the services. We do not guarantee the
+      completeness, honesty, accuracy, or reliability of any information or communications
+      submitted via our services, nor do we endorse any opinions expressed via the
+      services.You acknowledge that by using our services, you may be exposed to content
+      that is offensive, harmful, incorrect, or otherwise objectionable, as well as posts
+      that have been mislabeled or are otherwise misleading.All content is solely
+      responsible for the person who created it. We may not be able to monitor or control
+      the content that is posted through the services, and we cannot be held liable for
+      it. We reserve the right to remove content that violates the terms of service, such
+      as unlawful conduct, promoting self-harm or suicide, harassment, information
+      misdirection, or impersonation. Our help center has information on specific
+      policies(Safety and cybercrime)
+    </p>
+    <br />
+    <p class="header">Your rights and grant of rights in the content</p>
+    <p>
+      Any content you contribute, post, or display on or through our services remains your
+      property. You are solely responsible for the content you submitted, posted, or
+      displayed (Your incorporated audio, photos, videos and any other kind of submitted,
+      posted or displayed content on our services) By submitting, posting, or displaying
+      content on or through our services, you grant us a non-exclusive, royalty-free
+      license (with the right to sublicense) to use, copy, reproduce, process adapt,
+      modify, publish, transmit, display, and distribute such content in any and all media
+      now known or later developed (for clarity, these rights include for example,
+      curating, transforming, and translating). This license gives us permission to share
+      your material with the rest of the world.You agree that Travelity has the right to
+      provide, promote, and improve the services, as well as to make content submitted to
+      or through our services available to other companies, organizations, or individuals
+      for syndication, broadcast, distribution, sharing, promotion, or publication on
+      other media and services, subject to our terms and conditions for such content use.
+      Such additional uses by Travelity, or other companies, organizations, or
+      individuals, are made without payment to you with respect to content that you
+      submit, post, transmit, or otherwise make available through the services, as you
+      agree that your use of our services is sufficient compensation for the content and
+      grant of rights herein. Travelity's regulations for how partners can engage with
+      your content on our platforms are always changing.These guidelines are in place to
+      allow for an open environment that respects your rights.You acknowledge that we and
+      our partners may alter or adapt your material as it is distributed, syndicated,
+      published, or broadcast, and/or make changes to your content to adapt it to
+      different media. You represent and warrant that you have all necessary rights,
+      licenses, consents, permissions, power, and/or authority to grant the rights granted
+      herein for any content that you submit, post, or display on or through our services,
+      or that you have obtained all necessary rights, licenses, consents, permissions,
+      power, and/or authority. Unless you have the appropriate authorization or are
+      otherwise legally permitted to upload the material and grant Travelity the license
+      specified above, you agree that such content will contain material subject to
+      copyright or other intellectual rights.
+    </p>
+    <br />
+    <p class="header">Using our services</p>
+    <p>
+      Our services are always changing. As a result, the Services may alter at our
+      discretion from time to time. We may stop offering the Services or any features
+      within the Services to you or to users in general (permanently or temporarily). We
+      also reserve the right, in our sole discretion, to impose usage and storage
+      restrictions at any time. Without , we may also remove or refuse to distribute any
+      Content on the Services, limit the distribution or visibility of any Content on the
+      Service, suspend or remove users, and reclaim usernames without liability to you.
+      Using one of these services, you agree to any extra terms that apply to that
+      service. In return for Travelity granting you access and to use our services, you
+      agree that Travelity and its third-party providers and partners may use/place
+      advertisements on our platform to display content or information from the services
+      whether contributed by you or others, in exchange for Travelity giving you access to
+      and use of the Services. You also undertake not to abuse our Services by interfering
+      with them or accessing them in any way other than through the interface.
+    </p>
+    <br />
+    <p class="header">Your Account</p>
+    <p>
+      You are responsible for the security of your account, we recommend that you choose a
+      strong password and only use it for this one. We cannot and will not be held liable
+      for the loss or damage caused by you. We do not recommend sharing your password with
+      anyone that you don’t trust that this can violate our policy.
+    </p>
+    <br />
+    <p class="header">
+      Consent to receive communications about the user's actions, misbehavior, or
+      misconduct
+    </p>
+    <p>
+      a. You agree that we may contact you at the given email address you have provided
+      when you registered, to inform you about any form of actions, misbehavior or
+      misconduct that is not acceptable to display on the Service. b. You agree to be that
+      owner for the given email address that has been used to register to the Service or
+      have been authorized to use and grant others permissions to contact that email
+      address. c. You agree to be the person of whom you say to be and to stand reliable
+      for any of the actions, misbehavior or misconduct that the user may have posted on
+      our services. d. You agree that the Service can contact any form of local authority,
+      (child-protection services, self-harm hotline, police, animal control or any other
+      kinds) with any kind of evidence that has been provided by the user of the account.
+    </p>
+    <br />
+    <p class="header">Your Privacy</p>
+    <p>
+      Please read our Privacy Policy. We do not recommend that you share nonpublic
+      information about yourself, if you do share information regarding yourself we will
+      not stand reliable for any damages that can result because of it. Only share
+      information that you are comfortable to share with others.
+    </p>
+    <br />
+    <p class="header">Reservation of Rights</p>
+    <p>
+      We reserve the right to request that you remove all links or any particular link to
+      our App. You approve to immediately remove all links to our App upon request. We
+      also reserve the right to amend these terms and conditions and its linking policy at
+      any time. By continuously linking to our App, you agree to be bound to and follow
+      these linking terms and conditions.
+    </p>
+    <br />
+    <p class="header">Removal of links from our App</p>
+    <p>
+      If you find any link on our App that is offensive for any reason, you are free to
+      contact and inform us any moment. We will consider requests to remove links but we
+      are not obligated to or so or to respond to you directly. We do not ensure that the
+      information on this website is correct, we do not warrant its completeness or
+      accuracy; nor do we promise to ensure that the website remains available or that the
+      material on the website is kept up to date.
+    </p>
+    <br />
+    <p class="header">License</p>
+    <p>
+      Unless otherwise stated, the Service and/or its licensors own the intellectual
+      property rights for all material on the Service. All intellectual property rights
+      are reserved. You may access this from the Service for your own personal use
+      subjected to restrictions set in these terms and conditions.
+      <ul>You must not:
+        <li>Republish material from the Service</li>
+        <li>Sell, rent or sub-license material from the Service</li>
+        <li>Reproduce, duplicate or copy material from the Service</li>
+        <li>Redistribute content from the Service</li>
+      </ul>
+      <br/>
+      This Agreement shall begin on the date hereof.
+      Parts of this app offer an opportunity for users to post and exchange opinions and information in certain areas of the website.
+      The Service does not filter, edit, publish or review Comments prior to their presence on the website.
+      Comments do not reflect the views and opinions of the Service, its agents and/or affiliates.
+      Comments reflect the views and opinions of the person who posts their views and opinions.
+      To the extent permitted by applicable laws, the Service shall not be liable for the Comments or for any liability,
+      damages or expenses caused and/or suffered as a result of any use of and/or posting of and/or appearance of the Comments on this website.
+      <br/>
+      <br/>
+      The Service reserves the right to monitor all Comments and to remove any Comments which can be considered inappropriate,
+      offensive or causes breach of these Terms and Conditions.
+      <ul>You warrant and represent that:
+        <li>You are entitled to post the Comments on our App and have all necessary licenses and consents to do so;</li>
+        <li>he Comments do not invade any intellectual property right, including without limitation copyright, patent or trademark of any third party;</li>
+        <li>The Comments do not contain any defamatory, libelous, offensive, indecent or otherwise unlawful material which is an invasion of privacy;</li>
+        <li>The Comments will not be used to solicit or promote business or custom or present commercial activities or unlawful activity.</li>
+      </ul>
+      You hereby grant the Service a non-exclusive license to use, reproduce, edit and authorize others to use, reproduce and edit any of your Comments in any and all forms, formats or media.
+    </p>
+  </div>
 </template>
 
 <script>
@@ -10,4 +216,27 @@ export default {
 };
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.terms {
+  background-color: white;
+  padding: 50px;
+}
+
+.underline {
+  text-decoration: underline;
+  text-decoration-color: black;
+}
+
+.bold {
+  font-weight: bold;
+}
+
+.header {
+  font-weight: bold;
+  font-size: 25px;
+}
+
+li {
+  margin-left: 50px;
+}
+</style>
