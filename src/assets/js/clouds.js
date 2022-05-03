09386773cf5b113d07e4c34d68db60b21387cd17
Lukas Stauersbøl <lukas.stauersbol@gmail.com>
2022-05-03 08:04:35 +0200

    docs: small doc update

--- src/assets/js/clouds.js
+++ src/assets/js/clouds.js
@@ -2,8 +2,9 @@ import * as klouds from 'klouds';
 
 /**
  * Creates kloud instances
- * @param {string} kloud1
- * @param {string} kloud2
+ * @param {string} kloud1 scene 1 element id
+ * @param {string} kloud2 scene 2 element id
+ *
  */
 export function createClouds(kloud1, kloud2) {
   klouds.create({
