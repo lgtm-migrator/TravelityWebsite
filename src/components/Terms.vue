ccc24f34d57bdda15f45918bf47ffacaa4f539bf
Lukas Stauersbøl <lukas.stauersbol@gmail.com>
2022-05-03 10:16:12 +0200

    fix(terms): added more text to terms

--- src/components/Terms.vue
+++ src/components/Terms.vue
@@ -1,4 +1,5 @@
 <template>
+  <BToM/>
   <div class="terms">
     <p class="header">Terms and Conditions</p>
     <p>
@@ -199,17 +200,101 @@
       offensive or causes breach of these Terms and Conditions.
       <ul>You warrant and represent that:
         <li>You are entitled to post the Comments on our App and have all necessary licenses and consents to do so;</li>
-        <li>he Comments do not invade any intellectual property right, including without limitation copyright, patent or trademark of any third party;</li>
+        <li>The Comments do not invade any intellectual property right, including without limitation copyright, patent or trademark of any third party;</li>
         <li>The Comments do not contain any defamatory, libelous, offensive, indecent or otherwise unlawful material which is an invasion of privacy;</li>
         <li>The Comments will not be used to solicit or promote business or custom or present commercial activities or unlawful activity.</li>
       </ul>
       You hereby grant the Service a non-exclusive license to use, reproduce, edit and authorize others to use, reproduce and edit any of your Comments in any and all forms, formats or media.
     </p>
+    <br>
+    <p class="header">Hyperlinking to our App</p>
+    <p>
+      <ul>The following organizations may link to our App without prior written approval:
+        <li>Government agencies;</li>
+        <li>Search engines;</li>
+        <li>News organizations;</li>
+        <li>Online directory distributors may link to our App in the same manner as they hyperlink to the Websites of other listed businesses; and</li>
+        <li>System wide Accredited Businesses except soliciting non-profit organizations, charity shopping malls, and charity fundraising groups which may not hyperlink to our Website.</li>
+      </ul>
+      These organizations may link to our home page, to publications or to other App information so long as the link:
+      (a) is not in any way deceptive;
+      (b) does not falsely imply sponsorship, endorsement or approval of the linking party and its products and/or services; and
+      (c) fits within the context of the linking party’s site.
+      <ul>We may consider and approve other link requests from the following types of organizations:
+        <li>Commonly-known consumer and/or business information sources;</li>
+        <li>dot.com community sites;</li>
+        <li>associations or other groups representing charities;</li>
+        <li>Online directory distributors;</li>
+        <li>Internet portals;</li>
+        <li>Accounting, law and consulting firms; and</li>
+        <li>Educational institutions and trade associations.</li>
+      </ul>
+        We will approve link requests from these organizations if we decide that:
+        (a) the link would not make us look unfavorably to ourselves or to our accredited businesses;
+        (b) the organization does not have any negative records with us;
+        (c) the benefit to us from the visibility of the hyperlink compensates the absence of Travelity; and
+        (d) the link is in the context of general resource information.
+        <br>
+        <br>
+        These organizations may link to our App so long as the link:
+        (a) is not in any way deceptive;
+        (b) does not falsely imply sponsorship, endorsement or approval of the linking party and its products or services; and
+        (c) fits within the context of the linking party’s site.
+        If you are one of the organizations listed in paragraph 2 above and are interested in linking to our App,
+        you must inform us by sending an email to the Service. Please include your name, your organization name,
+        contact information as well as the URL of your site, a list of any URLs from which you intend to link to our App,
+        and a list of the URLs on our site to which you would like to link. Wait 2-3 weeks for a response.
+        <br>
+        <br>
+        <ul>Approved organizations may hyperlink to our App as follows:
+          <li>By use of our corporate name; or</li>
+          <li>By use of the uniform resource locator being linked to; or</li>
+          <li>By use of any other description of our App being linked to that makes sense within the context and format of content on the linking party’s site.</li>
+        </ul>
+        No use of the Service's logo or other artwork will be allowed for linking absent a trademark license agreement.
+    </p>
+    <br>
+    <p class="header">iFrames</p>
+    <p>Without prior approval and written permission, you may not create frames around our Web Pages
+      that alter in any way the visual presentation or appearance of our App.
+    </p>
+    <br>
+    <p class="header">Content Liability</p>
+    <p>
+      We shall not be held responsible for any content that appears on your App.
+      You agree to protect and defend us against all claims that are rising on our App.
+      No link(s) should appear on any Website that may be interpreted as libelous, obscene or criminal,
+      or which infringes, otherwise violates, or advocates the infringement or other violation of, any third party rights.
+    </p>
+    <br>
+    <p class="header">Disclaimer</p>
+    <p>
+      <ul>To the maximum extent permitted by applicable law, we exclude all representations, warranties and
+        conditions relating to our App and the use of this website. Nothing in this disclaimer will:
+        <li>limit or exclude our or your liability for death or personal injury;</li>
+        <li>limit or exclude our or your liability for fraud or fraudulent misrepresentation;</li>
+        <li>limit any of our or your liabilities in any way that is not permitted under applicable law; or</li>
+        <li>exclude any of our or your liabilities that may not be excluded under applicable law.</li>
+      </ul>
+      <br>
+      <br>
+      The limitations and prohibitions of liability set in this Section and elsewhere in this disclaimer:
+      (a) are subject to the preceding paragraph; and
+      (b) govern all liabilities arising under the disclaimer, including liabilities arising in contract, in tort and for breach of statutory duty.
+      <br>
+      <br>
+      As long as the website and the information and services on the website are provided free of charge,
+      we will not be liable for any loss or damage of any nature.
+    </p>
   </div>
 </template>
 
 <script>
+import BToM from './BToM.vue'
 export default {
+  components: {
+    BToM,
+  },
   data: () => ({}),
   methods: {},
   mounted() {},
