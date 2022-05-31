6ffb94dce31f240d56e7ea65f3cbf5355e38b794
nicolaj1449 <nicolajs2000@gmail.com>
2022-05-31 12:02:19 +0200

    changes
    changes file name to please the compiler

---
+++ src/components/BToTComponent.vue
@@ -0,0 +1,82 @@
+<template>
+  <a id="back"><v-icon>mdi-arrow-up-thick</v-icon></a>
+</template>
+<script>
+export default {
+  data: () => ({}),
+
+  mounted() {
+    // Adds scroll listener
+    window.addEventListener("scroll", this.backToTop);
+  },
+
+  beforeUnmount() {
+    // Removes listener
+    window.removeEventListener("scroll", this.backToTop);
+  },
+  methods: {
+    // A function that will be called when the user clicks the button.
+    backToTop() {
+      // Variables
+      const btn = document.querySelector("#back");
+      const scrollPos = window.scrollY;
+
+      // Checks scrollPos
+      if (scrollPos > 300) {
+        btn.classList.add("btn-show");
+      } else {
+        btn.classList.remove("btn-show");
+      }
+
+      // Adds listener to button
+      btn.addEventListener("click", function () {
+        window.scrollTo({
+          top: 0,
+          behavior: "smooth",
+        });
+      });
+    },
+  },
+};
+</script>
+<style lang="scss" scoped>
+/*
+Back button
+*/
+#back {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-color: white;
+  width: 50px;
+  height: 50px;
+  border-radius: 30px;
+  position: fixed;
+  bottom: 50px;
+  right: 35px;
+  transition: background-color 0.3s, opacity 0.5s, visibility 0.5s;
+  opacity: 0;
+  visibility: hidden;
+  z-index: 5;
+}
+
+#back.btn-show {
+  opacity: 1;
+  visibility: visible;
+}
+
+#back:active {
+  cursor: pointer;
+  background-color: #d1dffd;
+  color: white;
+}
+
+#back:hover {
+  cursor: pointer;
+  background-color: #d1dffd;
+  color: white;
+}
+/*
+Back button
+*/
+</style>
